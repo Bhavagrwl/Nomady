149ecc0e39f7b37522c8a369942f4ca3e971108a
Bhavya Agrawal <bhavya.agrawal1107@gmail.com>
2025-12-20 18:40:02 +0530

    restructurred routes

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const Listing=require("./models/listing.js");
 const path=require("path");
 const methodOverride=require("method-override");
 const ejsMate=require("ejs-mate");
-const wrapAsync=require("./utils/wrapAsync.js");
 const ExpressError=require("./utils/ExpressError.js");
-const {listingSchema,reviewSchema}=require("./schema.js");
-const Review=require("./models/review.js");
-const { wrap } = require("module");
+const listings=require("./routes/listing.js");
+const reviews=require("./routes/review.js");
 
 const app = express();
 
@@ -38,105 +35,8 @@ app.get("/", (req, res) => {
     // res.send("Working");
 })
 
-const validateListing=(req,res,next)=>{
-    let {error}=listingSchema.validate(req.body);
-    if(error){
-        let errMsg=error.details.map((el)=>el.message).join(",");
-        throw new ExpressError(400,errMsg);
-    } else{
-        next();
-    }
-}
-
-const validateReview=(req,res,next)=>{
-    let {error}=reviewSchema.validate(req.body);
-    if(error){
-        let errMsg=error.details.map((el)=>el.message).join(",");
-        throw new ExpressError(400,errMsg);
-    } else{
-        next();
-    }
-}
-
-// Index Route
-app.get("/listings",wrapAsync(async (req,res)=>{
-    const allListings = await Listing.find({});
-    res.render("listings/index.ejs",{allListings});
-}))
-
-// new and create route
-app.get("/listings/new",(req,res)=>{
-    res.render("listings/new.ejs");
-})
-
-app.post("/listings",validateListing,wrapAsync(async (req,res)=>{
-    const newListing=new Listing(req.body.listing);
-    await newListing.save();
-    res.redirect("/listings");
-}))
-
-// Show Route
-app.get("/listings/:id", wrapAsync(async (req,res)=>{
-    let {id}=req.params;
-    let data=await Listing.findById(id).populate("reviews");
-    res.render("listings/show",{data});
-}))
-
-// edit and update route
-app.get("/listings/:id/edit",wrapAsync(async(req,res)=>{
-    let {id}=req.params;
-    const listing=await Listing.findById(id);
-    res.render("listings/edit",{listing});
-}))
-
-app.put("/listings/:id",validateListing,wrapAsync(async (req,res)=>{
-    let {id}=req.params;
-    await Listing.findByIdAndUpdate(id,{...req.body.listing});
-    res.redirect(`/listings/${id}`);
-}))
-
-// delete route
-app.delete("/listings/:id",wrapAsync(async (req,res)=>{
-    let {id}=req.params;
-    let deleted=await Listing.findByIdAndDelete(id);
-    console.log(deleted);
-    res.redirect("/listings");
-}))
-
-// Reviews
-// Post Review Route
-app.post("/listings/:id/reviews", validateReview ,wrapAsync(async(req,res)=>{
-    let listing = await Listing.findById(req.params.id);
-    let newReview = new Review(req.body.review);
-
-    listing.reviews.push(newReview);
-
-    await newReview.save();
-    await listing.save();
-
-    res.redirect(`/listings/${listing._id}`);
-}));
-
-// Delete Review Route
-app.delete("/listings/:id/reviews/:reviewId",wrapAsync(async(req,res)=>{
-    let {id,reviewId}=req.params;
-    await Listing.findByIdAndUpdate(id,{$pull:{reviews: reviewId}});
-    await Review.findByIdAndDelete(reviewId);
-    res.redirect(`/listings/${id}`);
-}));
-
-// app.get("/testListing",async (req,res)=>{
-//     let sampleListing=new Listing({
-//         title: "My New Villa",
-//         description: "Near beach",
-//         price: "12000",
-//         location: "Calangute, Goa",
-//         country: "India"
-//     });
-//     await sampleListing.save();
-//     console.log("Sample saved");
-//     res.send("Saved");
-// })
+app.use("/listings",listings);
+app.use("/listings/:id/reviews", reviews);
 
 app.use((req, res, next) => {
   next(new ExpressError(404, "Page Not Found"));
--- models/listing.js
+++ models/listing.js
@@ -14,8 +14,8 @@ const listingSchema=new Schema({
         filename: String,
         url:{
             type: String,
-            default: "https://unsplash.com/photos/people-sit-near-water-in-a-japanese-garden-GdlcNAU_PW4",
-            set: (v)=>v===""?"https://unsplash.com/photos/people-sit-near-water-in-a-japanese-garden-GdlcNAU_PW4":v
+            default: "https://images.unsplash.com/photo-1749569338802-29bec39cb5d4?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            set: (v)=>v===""?"https://images.unsplash.com/photo-1749569338802-29bec39cb5d4?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D ":v
         }
     },
     price: Number,
